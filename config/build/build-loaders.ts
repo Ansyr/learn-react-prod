eb4bdec0e68b5776ced70ab6466c2457c3f8edfb
ansyr <vasutin2003@gmail.com>
2023-08-29 20:11:20 +0300

    fix webpack config

--- config/build/build-loaders.ts
+++ config/build/build-loaders.ts
@@ -6,11 +6,11 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
     const svgLoader = {
         test: /\.svg$/,
-        use: ['@svgr/webpack'],
+        use: ["@svgr/webpack"],
     }
 
     const fileLoader = {
-        test: /\.(png|jpe?g|gif|woff2|svg)$/i,
+        test: /\.(png|jpe?g|gif|woff2)$/i,
         use: [
             {
                 loader: 'file-loader',
